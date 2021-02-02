abf1cc6134e2564dc9fdff735e21f074a664782a
closeplanet2 <filerjosh@hotmail.com>
2021-02-02 14:55:12 +0000

    Base Motor Code

--- 1_Setup.ino
+++ 1_Setup.ino
@@ -1,7 +1,17 @@
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
+  
   pinMode(13, OUTPUT);
+  servoOne.attach(30);
+  servoTwo.attach(31);
+  servoThree.attach(32);
+  servoFour.attach(33);
+  servoFive.attach(34);
+  servoSix.attach(35);
+  servoSeven.attach(36);
+  servoEight.attach(37);
+  servoNine.attach(38);
 
   buttonOne.attachClick(Button1);
 }
--- ArdunioCode2.ino
+++ ArdunioCode2.ino
@@ -1,8 +1,19 @@
+#include <Servo.h>
 #include <SoftwareSerial.h>
 #include <OneButton.h>
 
 SoftwareSerial mySerial(11, 12);
 OneButton buttonOne(53, true); 
+
+Servo servoOne;
+Servo servoTwo;
+Servo servoThree;
+Servo servoFour;
+Servo servoFive;
+Servo servoSix;
+Servo servoSeven;
+Servo servoEight;
+Servo servoNine;
 
 int distanceList[] = {100,80,60,40,20,0,0};
 int currentPump = 0;
